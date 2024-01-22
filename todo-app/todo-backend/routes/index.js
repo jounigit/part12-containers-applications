391cc89fb436d750189906836c4254b7b5f5e562
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2024-01-22 15:06:01 +0200

    Exercise 12.10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -3,18 +3,26 @@ const redis = require('../redis');
 const router = express.Router();
 
 const configs = require('../util/config')
-console.log('CONFIG: ', configs)
 
 let visits = 0
 
 /* GET index data. */
 router.get('/', async (req, res) => {
   visits++
+  redis.setAsync("added_todos", visits)
+  let addedTodos = await redis.getAsync("added_todos") || '0'
+  console.log('Redis get: ', addedTodos)
 
   res.send({
     ...configs,
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  let visits = await redis.getAsync("added_todos") || 0
+
+  res.json({ "added_todos": Number(visits) })
+})
 
 module.exports = router;
